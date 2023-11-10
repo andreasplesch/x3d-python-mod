f6b356ce9f16333c3185f6ce425372e6d1317379
Andreas Plesch <andreasplesch@users.noreply.github.com>
2023-11-10 10:13:41 -0500

    Create swap_USEbeforeDEF.py

---
+++ test/swap_USEbeforeDEF.py
@@ -0,0 +1,58 @@
+from x3dkw import *
+import copy
+
+USEdict = {}
+DEFdict = {}
+USEbeforeDEFdict = {}
+
+def swap_USEbeforeDEF(node=None, parent=None):
+    USE = getattr(node,'USE', None)
+    if USE:
+        if USE not in USEdict:
+            USEdict[USE] = [{'node': node, 'parent': parent}]
+        else:
+            USEdict[USE].append({'node': node, 'parent': parent})
+        print(node.NAME(), 'USE: ' + USE + str(len(USEdict[USE])))
+        if USE not in DEFdict:
+            print ("USE " + USE + " occurred before DEF")
+            USEbeforeDEFdict[USE] = USEdict[USE] # record in dict
+    DEF = getattr(node,'DEF', None)
+    if DEF:
+        if DEF not in DEFdict: DEFdict[DEF] = [node]
+        else:
+            DEFdict[DEF].append(node)
+            print ("DEF " + DEF + " already defined")
+        print(node.NAME(),'DEF: ' + DEF + str(len(DEFdict[DEF])))
+        if DEF in USEbeforeDEFdict:
+            USEnode = USEbeforeDEFdict[DEF][0] # replace first USE node only
+            USEcopy = copy.deepcopy(USEnode['node'])
+            if len(DEFdict[DEF]) == 1: # only for first DEF node
+                print("replacing earlier USE with this DEF")
+                insertUSEIndex = USEnode['parent'].children.index(USEnode['node'])
+                USEnode['parent'].children[insertUSEIndex] = node
+            # DEF to USE
+            print("replacing DEF with copy of earlier USE")
+            insertDEFIndex = parent.children.index(node)
+            parent.children[insertDEFIndex] = USEcopy
+    for each in node.children:
+        swap_USEbeforeDEF(node=each, parent=node)
+
+x3d = X3D()
+scene = Scene()
+x3d.Scene = scene
+grpa = Group(DEF='A')
+grpb = Group(DEF='B')
+scene.children.append(grpa)
+scene.children.append(grpb)
+trans = Transform(DEF='C')
+trans2 = Transform(USE='C')
+grpb.children.append(trans)
+grpa.children.append(trans2)
+grpb.children.append(trans)
+print (x3d.XML())
+swap_USEbeforeDEF(node=scene)
+print (x3d.XML())
+#blob = x3d.XML()
+#fp = open("dump_case2.x3d","w+")
+#fp.write(blob)
+#fp.close()
