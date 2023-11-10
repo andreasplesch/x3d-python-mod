a9863d27e065892d0ee07c52104d10c68aad1551
Andreas Plesch <andreasplesch@users.noreply.github.com>
2023-11-10 11:13:24 -0500

    Update swap_USEbeforeDEF.py

--- test/swap_USEbeforeDEF.py
+++ test/swap_USEbeforeDEF.py
@@ -5,6 +5,9 @@ USEdict = {}
 DEFdict = {}
 USEbeforeDEFdict = {}
 
+#shapeclass = globals()['Shape']
+#print (shapeclass())
+
 def swap_USEbeforeDEF(node=None, parent=None):
     USE = getattr(node,'USE', None)
     if USE:
@@ -22,6 +25,13 @@ def swap_USEbeforeDEF(node=None, parent=None):
         else:
             DEFdict[DEF].append(node)
             print ("DEF " + DEF + " already defined")
+            if DEF not in USEbeforeDEFdict:
+                print ("creating a new USE node of the same type")
+                insertDEFIndex = parent.children.index(node)
+                nodeClass = node.NAME()
+                node = globals()[nodeClass](USE=DEF) # a bit hackish
+                parent.children[insertDEFIndex] = node
+                # use node.FIELD_DEFINITIONS to reset all fields to defaults
         print(node.NAME(),'DEF: ' + DEF + str(len(DEFdict[DEF])))
         if DEF in USEbeforeDEFdict:
             USEnode = USEbeforeDEFdict[DEF][0] # replace first USE node only
@@ -52,6 +62,29 @@ grpb.children.append(trans)
 print (x3d.XML())
 swap_USEbeforeDEF(node=scene)
 print (x3d.XML())
+
+USEdict = {}
+DEFdict = {}
+USEbeforeDEFdict = {}
+
+x3d = X3D()
+scene = Scene()
+x3d.Scene = scene
+grpa = Group(DEF='A')
+grpb = Group(DEF='B')
+scene.children.append(grpa)
+scene.children.append(grpb)
+trans = Transform(DEF='C')
+grpd = Group(DEF='D')
+trans.children.append(grpd)
+grpb.children.append(trans)
+grpa.children.append(trans)
+
+print (x3d.XML())
+swap_USEbeforeDEF(node=scene)
+print (x3d.XML())
+
+
 #blob = x3d.XML()
 #fp = open("dump_case2.x3d","w+")
 #fp.write(blob)
